db368cbf4bd980384e0fce2db28f64d20ccdbdb3
Ezeoke Chiamaka <aezeokeamaka@gmail.com>
2022-02-18 22:39:10 +0100

    created main.js
    Added functionality

---
+++ AdditionApp/main.js
@@ -0,0 +1,6 @@
+function myfunction(){
+    var num1 = parseInt(document.getElementById("first").value);
+    var num2 = parseInt(document.getElementById("second").value);
+    var total = num1 + num2
+    document.getElementById("Check").click();
+}
